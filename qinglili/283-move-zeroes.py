b04e27a2c20fc4c9adcc19ea5fd70c22d0767d35
qingni <lq15301353696@gmail.com>
2018-05-23 20:17:04 +0800

    Update 283-move-zeroes.py

--- qinglili/283-move-zeroes.py
+++ qinglili/283-move-zeroes.py
@@ -10,3 +10,23 @@
 # Note:
 # You must do this in-place without making a copy of the array.
 # Minimize the total number of operations.
+class Solution:
+    def movezeroes(self, nums: list):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)-1, -1, -1):
+            if i == len(nums) - 1 and nums[i] == 0:
+                continue
+            elif nums[i] == 0:
+                tmp = nums[i]
+                nums.pop(i)
+                nums.append(tmp)
+
+        return nums
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.movezeroes([0, 0, 3, 8, 0, 12])
+    print(r)
