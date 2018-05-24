378827d2fd0d4cc5c75fd5f574448589c481ffc1
qinglili <qinglili@tencent.com>
2018-05-24 18:25:42 +0800

    commit TwoSum

--- qinglili/167-Two-Sum.py
+++ qinglili/167-Two-Sum.py
@@ -9,3 +9,22 @@
 # 1.Your returned answers (both index1 and index2) are not zero-based.
 # 2.You may assume that each input would have exactly one solution and you may not use the same element twice.
 
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :param numbers: list[int]
+        :param trget: int
+        :return: list[int]
+        """
+        tmpDict = {}
+        for i in range(len(numbers)):
+            item = numbers[i]
+            if (target - item) in tmpDict:
+                return (tmpDict[target-item] + 1, i + 1)
+            tmpDict[item] = i
+
+
+if __name__ == '__main__':
+    s = Solution()
+    l = s.twoSum([2, 7, 11, 15],9)
+    print(l)
