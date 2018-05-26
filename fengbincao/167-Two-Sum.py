c4ea0fe7d3d2ccafc38f9c35f8954f7f7c0c427e
caofengbin <1050430934@qq.com>
2018-05-26 17:36:24 +0800

    implements twoSum

--- fengbincao/167-Two-Sum.py
+++ fengbincao/167-Two-Sum.py
@@ -9,3 +9,27 @@
 # 1.Your returned answers (both index1 and index2) are not zero-based.
 # 2.You may assume that each input would have exactly one solution and you may not use the same element twice.
 
+
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        beginPosition = 0
+        endPosition = len(numbers) - 1
+        while beginPosition < endPosition:
+            tmp = numbers[beginPosition] + numbers[endPosition]
+            if tmp == target:
+                return [beginPosition + 1, endPosition + 1]
+            elif tmp > target:
+                endPosition -= 1
+            elif tmp < target:
+                beginPosition += 1
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.twoSum([0, 1, 0, 3, 12], 4)
+    print(r)
