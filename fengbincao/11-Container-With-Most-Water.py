a94e3a251d776398a9fa72f1830ca918f40ae3f6
caofengbin <1050430934@qq.com>
2018-05-27 17:30:00 +0800

    add title container-with-most-water

---
+++ fengbincao/11-Container-With-Most-Water.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/container-with-most-water/description/
+# 题目大意：
+# 给定一个数组，数组元素的值表示竖线的高度，找两条竖线然后这两条线以及X轴构成的容器能容纳最多的水。
+
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
