4c993ce8cdd30f30a3bf2b799b19efda02988500
caofengbin <1050430934@qq.com>
2018-05-28 14:25:52 +0800

    implements container-with-most-water

--- fengbincao/11-Container-With-Most-Water.py
+++ fengbincao/11-Container-With-Most-Water.py
@@ -8,3 +8,37 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
+        # leftBar,riehtBar分别表示左边和右边最高的柱子，目标是寻找到leftBar左边没有比他高的柱子
+        # riehtBar右边没有比他高的柱子
+        leftBar = 0
+        rightBar = len(height) - 1
+
+        # 分别寻找的左边最高的柱子，以及右边最高的柱子
+        leftMax = height[leftBar]
+        rightMax = height[rightBar]
+
+        result = -1
+        maxArea = -1
+
+        while leftBar < rightBar:
+            if leftMax < rightMax:
+                maxArea = (rightBar - leftBar) * leftMax
+                leftBar += 1
+                if leftMax < height[leftBar]:
+                    leftMax = height[leftBar]
+            else:
+                maxArea = (rightBar - leftBar) * rightMax
+                rightBar -= 1
+                if rightMax < height[rightBar]:
+                    rightMax = height[rightBar]
+
+            if maxArea > result:
+                result = maxArea
+
+        return result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.maxArea([0, 1, 0, 3, 12])
+    print(r)
