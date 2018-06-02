adc99d1cd696a52ea43b3b34f4fcabd76eeea8f2
caofengbin <1050430934@qq.com>
2018-06-02 18:28:17 +0800

    implement lengthOfLongestSubstring

--- fengbincao/3-Longest-Substring-Without-Repeating-Characters.py
+++ fengbincao/3-Longest-Substring-Without-Repeating-Characters.py
@@ -13,8 +13,35 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        left_window = 0
+        right_window = -1
+        result = -1
+        statistics = [0] * 256
+        cahr_list = list(s)
+
+        while left_window < len(s):
+            if right_window + 1 < len(s) and statistics[ord(cahr_list[right_window + 1])] == 0:
+                right_window += 1
+                index = ord(cahr_list[right_window])
+                statistics[index] += 1
+            else:
+                index = ord(cahr_list[left_window])
+                statistics[index] -= 1
+                left_window += 1
+
+            result = max(result, right_window - left_window + 1)
+
+        if result == -1:
+            return 0
+        else:
+            return result
+
 
 if __name__ == '__main__':
     s = Solution()
-    r = s.lengthOfLongestSubstring('')
-    print(r)
+    r1 = s.lengthOfLongestSubstring('pwwkew')
+    print(r1)
+    r2 = s.lengthOfLongestSubstring('bbbbb')
+    print(r2)
+    r3 = s.lengthOfLongestSubstring('abcabcbb')
+    print(r3)
